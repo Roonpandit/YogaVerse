17b59a8341a1b0f9f09e2afb105116adf702a3f0
Tarun <tarunvashisth0000@gmail.com>
2025-03-09 01:24:07 +0530

    YogaVerse

--- src/components/box/Navbar.jsx
+++ src/components/box/Navbar.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../../assets/Logo-Yogaverse.png";
 
 function Navbar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <nav className="navbars">
       <div className="navbars-container">
@@ -12,7 +19,9 @@ function Navbar() {
             <span>YogaVerse</span>
           </Link>
         </div>
-        <div className="nav-buttons">
+        
+        {/* Desktop navigation buttons */}
+        <div className="nav-buttons desktop-nav">
           <Link to="/login" className="nav-button-signup">
             Login
           </Link>
@@ -20,7 +29,31 @@ function Navbar() {
             Contact Us
           </Link>
         </div>
+        
+        {/* Mobile menu hamburger icon */}
+        <div className="mobile-menu-icon" onClick={toggleSidebar}>
+          <div className={`hamburger ${sidebarOpen ? 'active' : ''}`}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </div>
+
+      {/* Mobile sidebar */}
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        <div className="sidebar-content">
+          <Link to="/login" className="sidebar-link" onClick={toggleSidebar}>
+            Login
+          </Link>
+          <Link to="/contact" className="sidebar-link" onClick={toggleSidebar}>
+            Contact Us
+          </Link>
+        </div>
+      </div>
+      
+      {/* Overlay when sidebar is open */}
+      {sidebarOpen && <div className="sidebar-overlay" onClick={toggleSidebar}></div>}
     </nav>
   );
 }
